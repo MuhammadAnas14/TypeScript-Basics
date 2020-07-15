e8c9af14638ca587a53b20d6f676833d62775293
Anas452 <anasm9877@gmail.com>
2020-07-15 23:52:56 +0500

    typeof

--- src/sample2.ts
+++ src/sample2.ts
@@ -6,4 +6,11 @@ function isEven(num:number){
 
  }
 
- console.log(isEven(10));
+console.log(isEven(10));
+
+let a: unknown = 30
+let b = a === 123
+// let c = a + 10
+if (typeof a === 'number') {
+    let d = a + 10
+}
