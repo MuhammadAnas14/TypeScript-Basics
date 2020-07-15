efe8cf05e4979012910650175cf2bb84236dcfa5
Anas452 <anasm9877@gmail.com>
2020-07-15 23:48:39 +0500

    basics

---
+++ build/sample2.js
@@ -0,0 +1,7 @@
+"use strict";
+var isActive = true;
+document.write("hello from ts");
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(10));
---
+++ src/sample.ts
@@ -0,0 +1,23 @@
+console.log('hello world')
+
+// let a= 1;
+// a = true; //once the type is define it can't be change
+
+// let std = {};
+// console.log(std.name) //name does not exist
+
+// function a(b:number){
+//   return b/2
+// }
+// a(3)
+// a("z")
+
+// let firstname : string = "Ali";
+// let lastname : string = "Ahmed";
+// let age : number = 21;
+// let isActiveStudent :boolean = true;
+// let courses: string[] = ["react" ,"JS", "TS"] 
+
+// let fullname = firstname + lastname;
+// let name = firstname + age;
+
